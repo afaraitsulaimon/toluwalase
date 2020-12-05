767340aaec1c2758a8f410d07e85f62695ed8d1e
Afarait Sulaimon Bola <afaraitsulaimon@gmail.com>
2020-12-05 17:47:55 +0100

    done with header and upper menu

---
+++ public/header-menu.php
@@ -0,0 +1,63 @@
+
+
+
+<!-- top header started from here-->
+<div class="container-fluid">
+         <div class="row">
+              <div class="col-12 top-header bg-dark">
+              <nav class="navbar navbar-light navbar-expand-md">
+					
+      
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#" class="text-light pl-3"><span class="glyphicon glyphicon-phone"></span> 08186657295</a></li>
+                        <li><a href="#" class="text-light pl-3"><span class="glyphicon glyphicon-envelope"></span> info@toluwalase.com.ng</a></li>
+                    </ul>
+					
+
+					<!-- Navbar links -->
+					<div class="collapse navbar-collapse left-top-header d-none d-lg-block d-xl-none" id="collapseTheTopHeader">
+						<ul class="navbar-nav text-white ml-auto nav-list">
+                  <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                  <li><a href="#" class="pl-4"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+							
+						</ul>
+					</div>
+			  </nav>
+              </div>
+         </div>
+    </div>
+<!-- top header ends here-->
+
+
+
+
+<!-- Menu started from here-->
+<div class="container-fluid">
+         <div class="row">
+              <div class="col-12 menu">
+              <nav class="navbar navbar-light navbar-expand-md">
+					       <a href="index.php" class="text-decoration-none" style="font-size:35px;font-weight:bold;">TOLUWALASE</a>
+
+					<button class="navbar-toggler navbar-light" data-toggle="collapse" data-target="#collapsibleNavbar">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+
+					<!-- Navbar links -->
+					<div class="collapse navbar-collapse left-top-header menu-list" id="collapsibleNavbar" >
+						<ul class="nav navbar-nav text-white ml-auto ">
+                  <li><a href="#" class="pl-3 text-decoration-none"><span class="glyphicon glyphicon-user"></span> Shop</a></li>
+                  <li><a href="#" class="pl-3 text-decoration-none"><span class="glyphicon glyphicon-log-in"></span> Top</a></li>
+                  <li><a href="#" class="pl-3 text-decoration-none"><span class="glyphicon glyphicon-user"></span> Skirt</a></li>
+                  <li><a href="#" class="pl-3 text-decoration-none"><span class="glyphicon glyphicon-log-in"></span> Dress</a></li>
+                  <li><a href="#" class="pl-3 text-decoration-none"><span class="glyphicon glyphicon-user"></span> Gown</a></li>
+                  <li><a href="#" class="pl-3 text-decoration-none"><span class="glyphicon glyphicon-log-in"></span> Jacket</a></li>
+                  <li><a href="#" class="pl-3 text-decoration-none"><span class="glyphicon glyphicon-user"></span> About Us</a></li>
+                  <li><a href="#" class="pl-3 text-decoration-none"><span class="glyphicon glyphicon-log-in"></span> Contact Us</a></li>
+							
+						</ul>
+					</div>
+			  </nav>
+              </div>
+         </div>
+    </div>
+<!-- menu ends here-->
