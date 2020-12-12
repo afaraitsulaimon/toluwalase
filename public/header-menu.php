1899332d03d8b0ec5745ad006962392438170baf
Afarait Sulaimon Bola <afaraitsulaimon@gmail.com>
2020-12-12 16:56:03 +0100

    created top, dress, jacket, gowns and skirt pages

--- public/header-menu.php
+++ public/header-menu.php
@@ -46,11 +46,11 @@
 					<div class="collapse navbar-collapse left-top-header menu-list" id="collapsibleNavbar" >
 						<ul class="nav navbar-nav text-white ml-auto ">
                   <li><a href="shop.php" class="pl-3 text-decoration-none"><span class="glyphicon glyphicon-user"></span> Shop</a></li>
-                  <li><a href="#" class="pl-3 text-decoration-none"><span class="glyphicon glyphicon-log-in"></span> Top</a></li>
-                  <li><a href="#" class="pl-3 text-decoration-none"><span class="glyphicon glyphicon-user"></span> Skirt</a></li>
-                  <li><a href="#" class="pl-3 text-decoration-none"><span class="glyphicon glyphicon-log-in"></span> Dress</a></li>
-                  <li><a href="#" class="pl-3 text-decoration-none"><span class="glyphicon glyphicon-user"></span> Gown</a></li>
-                  <li><a href="#" class="pl-3 text-decoration-none"><span class="glyphicon glyphicon-log-in"></span> Jacket</a></li>
+                  <li><a href="tops.php" class="pl-3 text-decoration-none"><span class="glyphicon glyphicon-log-in"></span> Top</a></li>
+                  <li><a href="skirt.php" class="pl-3 text-decoration-none"><span class="glyphicon glyphicon-user"></span> Skirt</a></li>
+                  <li><a href="dress.php" class="pl-3 text-decoration-none"><span class="glyphicon glyphicon-log-in"></span> Dress</a></li>
+                  <li><a href="gown.php" class="pl-3 text-decoration-none"><span class="glyphicon glyphicon-user"></span> Gown</a></li>
+                  <li><a href="jacket.php" class="pl-3 text-decoration-none"><span class="glyphicon glyphicon-log-in"></span> Jacket</a></li>
                   <li><a href="aboutus.php" class="pl-3 text-decoration-none"><span class="glyphicon glyphicon-user"></span> About Us</a></li>
                   <li><a href="contactus.php" class="pl-3 text-decoration-none"><span class="glyphicon glyphicon-log-in"></span> Contact Us</a></li>
 							
